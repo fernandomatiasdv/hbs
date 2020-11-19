b2e028c802ae0933da97bfd80bf28d1ddac1c555
Fernando del Valle <javascriptargentina@gmail.com>
2020-11-19 18:09:37 -0300

    jquery

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const app = express();
+const hbs = require('hbs');
 
-app.use(express.static(__dirname + '/public'))
+
+app.use(express.static(__dirname + '/public/'))
+
+hbs.registerPartials(__dirname+'/views/parciales');
+
 app.set('view engine' , 'hbs');
 
 app.get('/' , (req, res) => {
-    res.render('home')
+    res.render('home',{
+        anio: new Date().getFullYear(),
+        titulo: "Este es un titulo personalizado"
+    })
 })
 
 app.listen(3000, () => {
